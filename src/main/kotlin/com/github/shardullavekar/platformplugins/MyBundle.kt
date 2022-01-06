0a5437d3b1930d39a7c2e0078538f54e62c33b00
GitHub Action <action@github.com>
2022-01-06 10:12:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/shardullavekar/platformplugins/MyBundle.kt
+++ src/main/kotlin/com/github/shardullavekar/platformplugins/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.shardullavekar.platformplugins
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/shardullavekar/platformplugins/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.shardullavekar.platformplugins.services
+
+import com.github.shardullavekar.platformplugins.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
